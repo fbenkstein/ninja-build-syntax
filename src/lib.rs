455ee4ede7b4230abf166df822ef65f302da593a
Frank Benkstein <frank@benkstein.net>
2020-01-25 12:26:19 +0100

    move statements to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::convert::identity;
-use std::result;
 use std::str::from_utf8;
 
 use nom::{
@@ -12,8 +11,10 @@ use nom::{
 };
 
 mod error;
+mod statements;
 
 pub use error::Error;
+pub use statements::{parse, Statements};
 
 #[cfg(test)]
 #[macro_use]
@@ -709,62 +710,3 @@ fn test_comment() {
     test_parse!(comment(&b"#\n"[..]), Comment(&b""[..]));
 }
 
-#[derive(Debug)]
-pub enum Statement<'a> {
-    Rule(Rule<'a>),
-    Build(Build<'a>),
-    Binding(Binding<'a>),
-    Default(Default<'a>),
-    Include(Include<'a>),
-    Pool(Pool<'a>),
-    Comment(Comment<'a>),
-}
-
-fn statement(input: &[u8]) -> IResult<Statement> {
-    alt((
-        map(rule, Statement::Rule),
-        map(build, Statement::Build),
-        map(binding, Statement::Binding),
-        map(default, Statement::Default),
-        map(include, Statement::Include),
-        map(pool, Statement::Pool),
-        map(comment, Statement::Comment),
-    ))(input)
-}
-
-pub struct Statements<'a> {
-    data: &'a [u8],
-}
-
-pub fn parse(data: &[u8]) -> Statements {
-    Statements { data: data }
-}
-
-impl<'a> Iterator for Statements<'a> {
-    type Item = result::Result<Statement<'a>, Error>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if self.data.is_empty() {
-                return None;
-            }
-
-            let result = alt((map(statement, Some), map(line_ending, |_| None)))(self.data);
-
-            match result {
-                Ok((rest, statement)) => {
-                    self.data = rest;
-
-                    // Loop for empty lines.
-                    if let Some(statement) = statement {
-                        return Some(Ok(statement));
-                    }
-                }
-                Err(_) => {
-                    self.data = &b""[..];
-                    return Some(Err(Error::new()));
-                }
-            }
-        }
-    }
-}
